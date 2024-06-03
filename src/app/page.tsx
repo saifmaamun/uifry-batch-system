d1d0f392381b3c9c54906013d8c8ad807e602095
Saif Maamun <sadifmamun@gmail.com>
2024-06-03 19:46:47 +0600

    Faq done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Features from "./features/page";
 import FeatureSection from "@/components/ui/FeatureSection";
 import Advantages from "@/components/ui/Advantages";
 import Testimonial from "@/components/ui/Testimonial";
+import Faq from "@/components/ui/Faq";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <FeatureSection />
       <Advantages />
       <Testimonial />
+      <Faq />
     </>
   );
 };
---
+++ src/components/ui/Faq.tsx
@@ -0,0 +1,70 @@
+const Faq = () => {
+  return (
+    <div className="container mx-auto mb-16">
+      <div className="text-start">
+        <h2 className="text-lg font-medium text-red-600 uppercase">faq</h2>
+        <h2 className="text-5xl font-bold capitalize">
+          Frequently asked <br /> questions
+        </h2>
+      </div>
+      <div className="md:grid md:grid-cols-2 my-10 flex justify-center md:space-x-2 space-y-2 items-center align-middle">
+        <div className="mx-auto faq space-y-4 p-7">
+          <h2 className="text-3xl mb-3 font-semibold  capitalize">
+            the best financial accounting app ever!
+          </h2>
+          <h2 className="text-lg font-medium    capitalize ">
+            “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+            ultricies. In ultrices malesuada elit mauris.
+          </h2>
+        </div>
+        <div className="mx-auto space-y-4 p-7">
+          <h2 className="text-3xl mb-3 font-semibold  capitalize">
+            the best financial accounting app ever!
+          </h2>
+          <h2 className="text-lg font-medium   text-neutral-500 capitalize ">
+            “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+            ultricies. In ultrices malesuada elit mauris.
+          </h2>
+        </div>
+        <div className="mx-auto space-y-4 p-7">
+          <h2 className="text-3xl mb-3 font-semibold  capitalize">
+            the best financial accounting app ever!
+          </h2>
+          <h2 className="text-lg font-medium   text-neutral-500 capitalize ">
+            “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+            ultricies. In ultrices malesuada elit mauris.
+          </h2>
+        </div>
+        <div className="mx-auto faq space-y-4 p-7">
+          <h2 className="text-3xl mb-3 font-semibold  capitalize">
+            the best financial accounting app ever!
+          </h2>
+          <h2 className="text-lg font-medium    capitalize ">
+            “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+            ultricies. In ultrices malesuada elit mauris.
+          </h2>
+        </div>
+        <div className="mx-auto faq space-y-4 p-7">
+          <h2 className="text-3xl mb-3 font-semibold  capitalize">
+            the best financial accounting app ever!
+          </h2>
+          <h2 className="text-lg font-medium    capitalize ">
+            “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+            ultricies. In ultrices malesuada elit mauris.
+          </h2>
+        </div>
+        <div className="mx-auto space-y-4 p-7">
+          <h2 className="text-3xl mb-3 font-semibold  capitalize">
+            the best financial accounting app ever!
+          </h2>
+          <h2 className="text-lg font-medium   text-neutral-500 capitalize ">
+            “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+            ultricies. In ultrices malesuada elit mauris.
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Faq;
